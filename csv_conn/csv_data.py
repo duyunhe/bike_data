6bf4dc80a2d796dcc1a28627edd4ccdfb3fda819
bike <a_crux@126.com>
2018-03-05 12:35:14 +0800

    csv

--- csv_conn/csv_data.py
+++ csv_conn/csv_data.py
@@ -16,40 +16,40 @@ def read_title(line):
     return items
 
 
-def get_data_daily(filename, state='all'):
+def get_data_daily(filename, date_format, state='all'):
     """
 
     :param filename: 文件名
     :param state:  借还车状态: '0'代表借车 '1'代表未借车 默认为'all'代表所有状态
-    :return: {data} 按日期为key的每日信息列表
+    :param date_format: 
+    :return: 文件列表
     """
     fp = open(filename, 'r')
-
     data = {}
     line = fp.readline()
     var = locals()
     var_name = read_title(line)
-
-    for line in fp.readlines():
-        temp = line.strip('\n').split(',')
-        items = [one.strip('\"') for one in temp]
-        for name, item in zip(var_name, items):
-            var[name] = item
-        if state != 'all' and var['State'] != state:
-            continue
-        bid = "{0}:{1}".format(var['CompanyId'], var['BicycleNo'])
-        str_time = var['PositionTime']
-        position_time = datetime.strptime(str_time, '%Y/%m/%d %H:%M:%S')
-
-        day_time = position_time.strftime('%Y/%m/%d')
-        try:
-            data[day_time].append(items)
-        except KeyError:
-            data[day_time] = []
-            data[day_time].append(items)
-
     fp.close()
-    return data
+
+    idx = 0
+    with open(filename, 'rb') as fp:
+        for line in fp:
+            if idx == 0:
+                idx += 1
+                continue
+            temp = line.strip('\n').split(',')
+            items = [one.strip('\"') for one in temp]
+            # for name, item in zip(var_name, items):
+            #     var[name] = item
+            if state != 'all' and items[6] != state:
+                continue
+            bid = "{0}:{1}".format(items[0], items[1])
+            str_time = items[2]
+            position_time = datetime.strptime(str_time, date_format)
+            day_time = position_time.strftime('%Y/%m/%d')
+            idx += 1
+            if idx % 100000 == 0:
+                print idx
 
 
 def get_data_by_bike(filename, date_format):
@@ -81,7 +81,7 @@ def get_data_by_bike(filename, date_format):
                 data[bid] = []
                 data[bid].append(items)
             idx += 1
-            if idx % 10000 == 0:
+            if idx % 100000 == 0:
                 print idx
 
     fp.close()
@@ -99,12 +99,12 @@ def get_data_by_day(filename, state='all'):
     for line in fp.readlines():
         temp = line.strip('\n').split(',')
         items = [one.strip('\"') for one in temp]
-        for name, item in zip(var_name, items):
-            var[name] = item
+        # for name, item in zip(var_name, items):
+        #     var[name] = item
         if state != 'all' and var['State'] != state:
             continue
-        bid = "{0}:{1}".format(var['CompanyId'], var['BicycleNo'])
-        str_time = var['PositionTime']
+        bid = "{0}:{1}".format(items[0], items[1])
+        str_time = items[2]
         position_time = datetime.strptime(str_time, '%Y/%m/%d %H:%M:%S')
 
         day_time = position_time.strftime('%Y/%m/%d')
@@ -122,22 +122,37 @@ def get_data_by_day(filename, state='all'):
     return data
 
 
-def get_bike_last_data_by_day(filename):
-    datas = get_data_by_day(filename)
-    bike_status_list = {}
-    for day in datas:
-        bike_status_list[day] = []
-        for bike, data_list in datas[day].items():
-            bike_status_list[day].append(datas[day][bike][-1])
-    return bike_status_list
-
-
 def get_bike_last_data(filename, date_format):
-    datas = get_data_by_bike(filename, date_format)
+    fp = open(filename, 'r')
+
+    data = {}
+    line = fp.readline()
+    var = locals()
+    var_name = read_title(line)
+
+    idx = 0
     bike_status_list = {}
-    for bid, data_list in datas.items():
-        bike_status_list[bid] = datas[bid][-1]
-    return bike_status_list
+    with open(filename, 'rb') as fp:
+        for line in fp:
+            if idx == 0:
+                idx = 1
+                continue
+            temp = line.strip('\n').split(',')
+            items = [one.strip('\"') for one in temp]
+            # for name, item in zip(var_name, items):
+            #     var[name] = item
+            bid = "{0}:{1}".format(items[0], items[1])
+            # str_time = var['PositionTime']
+            # position_time = datetime.strptime(str_time, date_format)
+
+            # day_time = position_time.strftime('%Y/%m/%d')
+            data[bid] = items
+            idx += 1
+            if idx % 1000000 == 0:
+                print idx
+
+    fp.close()
+    return data
 
 
 def get_bike_area(filename):
